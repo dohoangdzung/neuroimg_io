e5b288395bcb04b474ff26e95c9a5fa3ad6257f0
Dung Do <dzung.do@fruitful.io>
2019-09-03 23:53:16 -0400

    Fix result pulling script

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-scp -i ~/.ssh/simgridvm-key -p centos@206.167.182.108:/mnt/io_benchmark/export/ export/
+scp -i ~/.ssh/simgridvm-key -p centos@206.167.182.108:/mnt/io_benchmark/export/*.* export/
